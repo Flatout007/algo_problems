d92c85aabc4a0c125310cfa8add71fc7111332d1
flatout007 <thechamp301@gmail.com>
2021-09-20 10:11:57 -0400

    21. Merge Two Sorted Lists solution

--- leetcode_21.js
+++ leetcode_21.js
@@ -12,3 +12,54 @@ Example 2:
 Input: l1 = [], l2 = []
 Output: []
 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(l1, l2) {
+    let node = new MyListNode(-1);
+    let head = node;
+    
+    if(!l1) {
+        return l2;
+    } else if(!l2) {
+      return l1;
+    } else if (!l1 && !l2) return l1;
+    
+    // the trick is to use a blank node to 
+    // cycle through and comparison sort the list.
+    while(l1 && l2) {
+        if(l1.val < l2.val) {
+            node.next = l1;
+            l1 = l1.next;
+        } else if(l2.val < l1.val) {
+            node.next = l2;
+            l2 = l2.next;
+        } else {
+            node.next = l1;
+            l1 = l1.next;  
+        }
+        
+        node = node.next;
+    }
+    
+    if(!l1) node.next = l2;
+    if(!l2) node.next = l1;
+
+    return head.next;  
+};
+
+
+function MyListNode(val, next) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+}
