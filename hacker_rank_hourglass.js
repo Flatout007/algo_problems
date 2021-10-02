bb6679d6ad0e1b4843b94ff24ab2260b8fa6b726
flatout007 <thechamp301@gmail.com>
2021-10-01 22:55:50 -0400

    hacker rank hourglass

--- hacker_rank_hourglass.js
+++ hacker_rank_hourglass.js
@@ -1,3 +1,19 @@
+/*
+Given a  2D Array, :
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+An hourglass in  is a subset of values with indices falling in this pattern in 's graphical representation:
+a b c
+  d
+e f g
+There are  hourglasses in . An hourglass sum is the sum of an hourglass' values. 
+Calculate the hourglass sum for every hourglass in the 2d array, then print the max sum.
+*/
+
 function hourglassSum(arr) {
     let maxSum = -Infinity;
     let hourGlassSum = 0;
