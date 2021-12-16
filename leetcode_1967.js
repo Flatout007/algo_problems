3ac3cbe32ae8962fdc10741625fc119ec195a702
flatout007 <thechamp301@gmail.com>
2021-12-15 22:07:28 -0500

    1967. Number of Strings That Appear as Substrings in Word

--- leetcode_1967.js
+++ leetcode_1967.js
@@ -23,22 +23,14 @@ Explanation:
  * @return {number}
  */
  var numOfStrings = function(patterns, word) {
-    let count = 0; 
-    // not passing all test cases
-    for(let i = 0; i<word.length; i++) {
-        for(let j = 0; j<word.length; j++) {
-            if(patterns.includes(word.slice(i,j + 1))) {
-                count += 1;
-            }               
+    let count = 0;
+     
+    for(let i in patterns) {
+        if(word.includes(patterns[i])) {
+            count += 1;
         }
     }
      
     return count; 
+     
 };
-
-
-
-
-// p1 = 0
-// p2 = length of s - 1
-// if we dont find it on first iteration increment i by p2
