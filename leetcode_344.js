281187cf3094234c5ed3428a9df823faf44b4c75
flatout007 <thechamp301@gmail.com>
2022-01-07 20:42:07 -0500

    344. Reverse String[Easy]

---
+++ leetcode_344.js
@@ -0,0 +1,24 @@
+/*
+// 344. Reverse String[Easy]
+Write a function that reverses a string. 
+The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+*/
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+    for(let i = 0; i<s.length / 2; i++) {
+        s[i] = mutate(s[s.length - i - 1], s[s.length - i - 1] = s[i]);
+    }
+};
+
+var mutate = function(x, y) {
+    return x;
+};
+
