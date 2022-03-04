3d0477fbb3487c7366c15256fb315995647b4f71
Flatout007 <thechamp301@gmail.com>
2022-03-03 20:15:45 -0500

    2032. Two Out of Three[Easy]

--- leetcode_2032.js
+++ leetcode_2032.js
@@ -18,30 +18,24 @@ Explanation: The values that are present in at least two arrays are:
  * @param {number[]} nums3
  * @return {number[]}
  */
- var twoOutOfThree = function(nums1, nums2, nums3) {
-    // does not work, im'a try a nested loop to check array's instead
-    let output = [];
-    let maxArr = fun(nums1, nums2, nums3);
-    
-    nums2 = new Set(nums2), nums3 = new Set(nums3);
-    
-    for(let i = 0; i<maxArr.length; i++) {
-        if(nums2.has(maxArr[i]) || nums3.has(maxArr[i])) {
-            output.push(maxArr[i]);
-        }
-    }
-    
-   return [3,2,1];
+ var twoOutOfThree = function (nums1, nums2, nums3) {
+   let output = [];
+
+   // compare the arrays nums1 & nums2, nums2 & nums3, & finally nums1 & nums3
+   nums1.forEach((ele) => {
+     if (nums2.includes(ele) && !output.includes(ele)) output.push(ele);
+   });
+
+   nums2.forEach((ele) => {
+     if (nums3.includes(ele) && !output.includes(ele)) {
+       output.push(ele);
+     }
+   });
+   
+   nums3.forEach((ele) => {
+     if (nums1.includes(ele) && !output.includes(ele)) output.push(ele);
+   });
+
+   return output;
  };
-// get the longest arr
-var fun = function(arr1, arr2, arr3) {
-    let arrs = [arr1, arr2, arr3];
-    let max = arrs.shift();
-    for(let i = 0; i<arrs.length; i++) {
-        if(max.length < arrs[i].length) {
-            max = arrs[i];
-        }
-    }
-    return max;
-}
 
