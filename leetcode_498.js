98055ccabda0a4195e9776e131b4f25d2564793e
Flatout007 <thechamp301@gmail.com>
2022-04-06 22:05:52 -0400

    498. Diagonal Traverse [Medium]

---
+++ leetcode_498.js
@@ -0,0 +1,64 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
+ var findDiagonalOrder = function(arr) {
+    /*
+    1.SET an new array
+      (to fill with traversed diagaonls & return)
+    
+    2.TRAVERSE up then down diagonals, storing them & doing step#3
+      
+    3.UPDATE i & j after step#2 by increasing j if going up;
+      increasing i if going down;
+      (*IF out of bounds when updating going up, increase i)
+      (*IF out of bounds when updating going down, increase j)
+    */
+    
+    let mat = [];
+    let turn = 0;
+    let row = 0, col = 0, area = arr.length * arr[0].length;
+    
+    // Matrix is less than 2. Impossible to get diagaonals, so flatten matrix
+    if(arr.length<2 || arr[0].length<2) 
+        return arr.flat();
+    
+    while(arr.length < area) {
+        // alternate up,down by checking if even or odd
+        
+        if(turn % 2 === 0) {
+            
+            while (row > 0 && col < arr[0].length-1) {
+                mat.push(arr[row--][col++]);
+            }
+            
+            // get the final or single cell in diaganal
+            mat.push(arr[row][col]);
+
+            if (row == arr.length-1 && col == arr[0].length-1) break;
+
+            // puts row,col in position to get next diagonal
+            else if (col >= arr[0].length-1)
+                row++;
+            else col++;
+           
+        } else {
+            
+            while (row < arr.length-1 && col > 0) {
+                mat.push(arr[row++][col--]);
+            }
+        
+            mat.push(arr[row][col]);
+            
+            if (row == arr.length-1 && col == arr[0].length-1) break;
+
+            else if (row >= arr.length-1)
+                col++;
+            else row++;
+        }
+     
+        turn ++;   
+    }
+    
+    return mat;
+};
