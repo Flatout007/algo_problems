6be134473d5018209ca25e4a831ddd127fc913c7
Flatout007 <thechamp301@gmail.com>
2022-04-09 21:34:22 -0400

    704. Binary Search [Easy]

--- leetcode_704.js
+++ leetcode_704.js
@@ -1,39 +1,40 @@
 /**
  * @description @param {
  *   704. Binary Search [Easy]
- * 
- *   Given an array of integers nums which is sorted in ascending order, 
- *   and an integer target, write a function to search target in nums. 
- * 
+ *
+ *   Given an array of integers nums which is sorted in ascending order,
+ *   and an integer target, write a function to search target in nums.
+ *
  *   If target exists, then return its index. Otherwise, return -1.
  *   You must write an algorithm with O(log n) runtime complexity.
- * } 
- * 
+ * }
+ *
  * @example @param {
  *   Example 1:
- * 
+ *
  *   Input: nums = [-1,0,3,5,9,12], target = 9
  *   Output: 4
  *   Explanation: 9 exists in nums and its index is 4
- * }  
-*/
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
+ * }
  */
- var search = function(nums, target) {
-    let left = 0, right = nums.length-1;
-    
-    while(left<=right) {
-        let mid = left + (right - left / 2);
-        
-        if(nums[mid] === target) {
-            return mid;
-        } else if(nums[mid] < target) {
-            left = mid+1;
-        } else right = mid-1;
-    }
-    
-    return -1;
+/**
+ *  @param {number[]} nums
+ *  @param {number} target
+ *  @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + (right - left / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else right = mid - 1;
+  }
+
+  return -1;
 };
