614fd90a1b263ebc3af295e4816eb76b44d59acb
Flatout007 <thechamp301@gmail.com>
2022-04-09 21:30:48 -0400

    704. Binary Search [Easy]

--- leetcode_704.js
+++ leetcode_704.js
@@ -1,5 +1,7 @@
 /**
- * @description  @param {
+ * @description @param {
+ *   704. Binary Search [Easy]
+ * 
  *   Given an array of integers nums which is sorted in ascending order, 
  *   and an integer target, write a function to search target in nums. 
  * 
