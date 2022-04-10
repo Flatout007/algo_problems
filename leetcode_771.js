cfd08c1a189cda147a31947d2dd9f531f0f6ca9b
Flatout007 <thechamp301@gmail.com>
2022-04-09 21:17:23 -0400

    771. Jewels and Stone[Easy]

--- leetcode_771.js
+++ leetcode_771.js
@@ -1,12 +1,19 @@
-/*
-// 771. Jewels and Stones       [Easy]
-You're given strings jewels representing the types of stones that are jewels, and stones representing the 
-stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones
-you have are also jewels. Letters are case sensitive, so "a" is considered a different type of stone from "A".
-
-Example 1:
-Input: jewels = "aA", stones = "aAAbbbb"
-Output: 3
+/** 
+ * @description 
+ *  @param {
+ *   771. Jewels and Stone[Easy]
+ * 
+ *   You're given strings jewels representing the types of stones that are jewels, 
+ *   and stones representing the stones you have. Each character in stones is a type 
+ *   of stone you have. You want to know how many of the stones you have are also jewels.
+ *   Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ * }
+ * 
+ * @example {
+    Example 1:
+    Input: jewels = "aA", stones = "aAAbbbb"
+    Output: 3
+   }
 */
 /**
  * @param {string} jewels
