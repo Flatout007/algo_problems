1c4a599de365926ea83f9a9d63f464120a3e1e51
Flatout007 <thechamp301@gmail.com>
2022-04-10 21:01:35 -0400

    387. First Unique Character in a String[Easy]

--- leetcode_387.js
+++ leetcode_387.js
@@ -1,13 +1,17 @@
-/*
-// 387. First Unique Character in a String[Easy]
+/**
+@description 
+    @param {
+    387. First Unique Character in a String[Easy]
 
-Given a string s, find the first non-repeating character in it and 
-return its index. If it does not exist, return -1.
-
-Example 1:
-Input: s = "leetcode"
-Output: 0
-*/
+    Given a string s, find the first non-repeating character in it and 
+    return its index. If it does not exist, return -1.
+}
+@example 
+    @param {
+    Example 1:
+    Input: s = "leetcode"
+    Output: 0
+}
 /**
  * @param {string} s
  * @return {number}
