1e306b87ffaddf093dd0b34dddc83e861d3dd584
Flatout007 <thechamp301@gmail.com>
2022-04-16 21:13:14 -0400

    942. DI String Match[Easy]

--- leetcode_942.js
+++ leetcode_942.js
@@ -1,6 +1,7 @@
 /** 
 @description @param {
     942. DI String Match[Easy]
+
     A permutation perm of n + 1 integers of all the integers in the range [0, n] 
     can be represented as a string s of length n where:
     s[i] == 'I' if perm[i] < perm[i + 1], and
@@ -16,6 +17,7 @@
     Input: s = "IDID"
     Output: [0,4,1,3,2]
 }
+
 * @param {string} s
 * @return {number[]}
 */
