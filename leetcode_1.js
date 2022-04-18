ec5fcd71da10de450dd242f00a4653e18d9573a0
Flatout007 <thechamp301@gmail.com>
2022-04-17 22:18:44 -0400

    Two Sum LeetCode 1.[EASY]

--- leetcode_1.js
+++ leetcode_1.js
@@ -1,28 +1,26 @@
-/* 
-// 1. Two Sum   EASY
-Given an array of integers nums and an integer 
-target, return indices of the two numbers 
-such that they add up to target
-
-Example 
-input:([3,3], 6) output => [0,1];
-*/
-
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
- var twoSum = function(nums, target) {
-    // for(let i = 0; i<nums.length; i++) {
-    //     for(let j = i + 1; j<nums.length; j++) {
-    //         if(nums[i] + nums[j] === target) {
-    //             return [i,j];
-    //         } 
-    //     }
-    // }
+ @function twoSum(nums->number[],target->number)
 
-       
+ @namespace { Two Sum LeetCode 1.[EASY] }
+
+ @description @namespace {
+    Given an array of integers nums and an integer 
+    target, return indices of the two numbers 
+    such that they add up to target.
+ }
+
+ @example @namespace {
+    Example: 
+
+    input:([3,3], 6) 
+    output => [0,1]
+ }
+
+* @param {number[]} nums
+* @param {number} target
+* @return {number[]}
+*/
+ var twoSum = function(nums, target) {
  let hash = new Map();
 
  let arr = [];
