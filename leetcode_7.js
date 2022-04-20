295d5e78b7895a4197b4ffcfc06bf6684ea6ccab
Flatout007 <thechamp301@gmail.com>
2022-04-19 20:20:27 -0400

    7. Reverse Integer

--- leetcode_7.js
+++ leetcode_7.js
@@ -1,13 +1,14 @@
-/*
-    // 7. Reverse Integer
-    Given a signed 32-bit integer x, return x with its digits reversed. 
-    If reversing x causes the value to go outside the 
-    signed 32-bit integer range [-2^31, 2^(31) - 1], then return 0.
+/** 
+    @description @namespace {
+    7. Reverse Integer
+        Given a signed 32-bit integer x, return x with its digits reversed. 
+        If reversing x causes the value to go outside the 
+        signed 32-bit integer range [-2^31, 2^(31) - 1], then return 0.
+    }
 
-    // ex: input:(-123) output => -321;
-*/
-
-/**
+    @example @namespace {
+     example: input:(-123) output => -321;
+    }
  * @param {number} x
  * @return {number}
  */
