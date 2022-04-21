db69f5a3efedb6da442b66ca5f31650ea3a9518a
Flatout007 <thechamp301@gmail.com>
2022-04-20 21:42:45 -0400

    changes

--- leetcode_14.ts
+++ leetcode_14.ts
@@ -10,16 +10,5 @@ Input: strs = ["flower","flow","flight"]
 Output: "fl"
 */
 function longestCommonPrefix(strs: string[]): string {
-  let output = "";
-  
-  for(let i = 0; i<strs[0].length; i++) {
-      for(let string of strs) {
-         if(!string[i] || strs[0][i] !== string[i]) {
-             return output;
-         }
-      }
-      output += strs[0][i];
-  }
-  
-  return output;
+ 
 };
