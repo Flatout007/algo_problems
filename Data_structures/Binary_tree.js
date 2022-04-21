7a92c7a5cf4b17f23d8437a08e1972391e0d5a99
Flatout007 <thechamp301@gmail.com>
2022-04-21 18:04:09 -0400

    Binary tree

--- Data_structures/Binary_tree.js
+++ Data_structures/Binary_tree.js
@@ -19,91 +19,87 @@
 // Node prototype for binary tree.
 
 function Node() {
-    this.data = null;
-    this.left = null;
-    this.right = null;
+  this.data = null;
+  this.left = null;
+  this.right = null;
 }
 
 /**
  * @function createTree();
- * 
+ *
  * @algorithm {
  *  1.ALLOCATE memory for a new Node;
- * 
+ *
  *  2.SET & ask user input for node data;
- * 
+ *
  *  3.RECURSIVELY add left, right to tree;
  * }
  */
-Node.createTree = function() {
-    let newNode = new Node();
+Node.createTree = function () {
+  let newNode = new Node();
 
-    var val = prompt("enter number greater than -1");
+  var val = prompt("enter number greater than -1");
 
-    if(val < 0)
-        return null;
-    newNode.data = val;    
-    alert("enter left child of `${val}`");
-    newNode.left = Node.createTree();
-    alert("enter right child of `${val}`");
-    newNode.right = Node.createTree();
-    
-    return newNode;
-}
+  if (val < 0) return null;
+  newNode.data = val;
+  alert("enter left child of `${val}`");
+  newNode.left = Node.createTree();
+  alert("enter right child of `${val}`");
+  newNode.right = Node.createTree();
+
+  return newNode;
+};
 
 /**
  * @function inOrder(root->Node);
- * 
+ *
  * @algorithm {
  *   1.IF root equals null return to recursive call;
- *   
- *   2.RECURSIVELY get left, root, then right; 
+ *
+ *   2.RECURSIVELY get left, root, then right;
  * }
  */
-Node.inOrder = function(root) {
-    if(root === null) 
-        return;
-    Node.inOrder(root.left);
-    console.log(root.data);
-    Node.inOrder(root.right); 
-}
+Node.inOrder = function (root) {
+  if (root === null) return;
+  Node.inOrder(root.left);
+  console.log(root.data);
+  Node.inOrder(root.right);
+};
 /**
  * @function preOrder(root->Node);
- * 
+ *
  * @algorithm {
  *   1.IF root equals null return to recursive call;
- *   
- *   2.RECURSIVELY get root, left, then right; 
+ *
+ *   2.RECURSIVELY get root, left, then right;
  * }
  */
-Node.preOrder = function(root) {
-    if(root === null)
-        return;
-    console.log(root.data);
-    Node.preOrder(root.left);
-    Node.preOrder(root.right);
-}
+Node.preOrder = function (root) {
+  if (root === null) return;
+  console.log(root.data);
+  Node.preOrder(root.left);
+  Node.preOrder(root.right);
+};
 
 /**
  * @function postOrder(root->Node);
- * 
+ *
  * @algorithm {
  *   1.IF root equals null return to recursive call;
- *   
- *   2.RECURSIVELY get left, right, then root; 
+ *
+ *   2.RECURSIVELY get left, right, then root;
  * }
  */
-Node.postOrder = function(root) {
-    if(root === null) 
-        return;
-    console.log(root.data);
-    Node.postOrder(root.left);
-    Node.postOrder(root.right);
-}
+Node.postOrder = function (root) {
+  if (root === null) return;
+  console.log(root.data);
+  Node.postOrder(root.left);
+  Node.postOrder(root.right);
+};
 
-const init = function() {
-    const root = Node.createTree();
-    console.log(root);
-    Node.inOrder(root);
+const init = function () {
+  const root = Node.createTree();
+  console.log(root);
+  Node.inOrder(root);
 };
 init();
