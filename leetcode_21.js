1c2e34b20e8157b1ca1418c55442cc5fbf04e3d8
Flatout007 <thechamp301@gmail.com>
2022-04-28 22:46:33 -0400

    21. Merge Two Sorted Lists

--- leetcode_21.js
+++ leetcode_21.js
@@ -1,26 +1,24 @@
-/*
-//21. Merge Two Sorted Lists
+/** 
+@description @namespace {
+ 21. Merge Two Sorted Lists
 
-Merge two sorted linked lists and return it as a sorted list. 
-The list should be made by splicing together the nodes of the first two lists.
+    Merge two sorted linked lists and return it as a sorted list. 
+    The list should be made by splicing together the nodes of the first two lists.
 
-Example 1:
-Input: l1 = [1,2,4], l2 = [1,3,4]
-Output: [1,1,2,3,4,4]
+    Example 1:
+    Input: l1 = [1,2,4], l2 = [1,3,4]
+    Output: [1,1,2,3,4,4]
 
-Example 2:
-Input: l1 = [], l2 = []
-Output: []
-*/
-
-/**
+    Example 2:
+    Input: l1 = [], l2 = []
+    Output: []
+}
+@constructor {
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
+}
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
