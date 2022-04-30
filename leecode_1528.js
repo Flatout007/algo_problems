7af8f4b969356705ec9a99c37dc043c9ee4d94d1
Flatout007 <thechamp301@gmail.com>
2022-04-29 22:20:08 -0400

    1528. Shuffle String

--- leecode_1528.js
+++ leecode_1528.js
@@ -1,15 +1,15 @@
-/*
-// 1528. Shuffle String
-Given a string s and an integer array indices of the same length.
-The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
-Return the shuffled string.
-
-Example 1:
-Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
-Output: "leetcode"
-Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
-*/
-/**
+/** 
+@description @namespace {
+  1528. Shuffle String
+  Given a string s and an integer array indices of the same length.
+  The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+  Return the shuffled string.
+}
+@example {
+  Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+  Output: "leetcode"
+  Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+}
  * @param {string} s
  * @param {number[]} indices
  * @return {string}
