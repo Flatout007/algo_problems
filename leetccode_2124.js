82c1ce02950bfc8516242e17362c74fc409e3337
Flatout007 <thechamp301@gmail.com>
2022-04-29 22:21:50 -0400

    2124. Check if All A's Appears Before All B's [Easy]

--- leetccode_2124.js
+++ leetccode_2124.js
@@ -1,18 +1,19 @@
 /**
- * 2124. Check if All A's Appears Before All B's [Easy]
- 
-Given a string s consisting of only the characters 'a' and 'b',
-return true if every 'a' appears before every 'b' in the string.
-Otherwise, return false.
+ @description @namespace { 
+        2124. Check if All A's Appears Before All B's [Easy]
 
-Example 1:
-Input: s = "aaabbb"
-Output: true
-Explanation:
-The 'a's are at indices 0, 1, and 2, while the 'b's are at indices 3, 4, and 5.
-Hence, every 'a' appears before every 'b' and we return true.
- */
-/**
+    Given a string s consisting of only the characters 'a' and 'b',
+    return true if every 'a' appears before every 'b' in the string.
+    Otherwise, return false.
+ }
+
+@example {
+    Input: s = "aaabbb"
+    Output: true
+    Explanation:
+    The 'a's are at indices 0, 1, and 2, while the 'b's are at indices 3, 4, and 5.
+    Hence, every 'a' appears before every 'b' and we return true.
+}
  * @param {string} s
  * @return {boolean}
  */
