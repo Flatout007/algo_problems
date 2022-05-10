a8efd0e8a658e466a69ae9015f3448ea89e0c893
Flatout007 <thechamp301@gmail.com>
2022-05-09 21:17:22 -0400

    61. Rotate List

--- leetcode_61.js
+++ leetcode_61.js
@@ -1,22 +1,24 @@
 /**
 @description @namespace {
   61. Rotate List
-Given the head of a linked list, rotate the list to the right by k places.
+  Given the head of a linked list, 
+  rotate the list to the right by k places.
 }
 
 @example {
-Input: head = [1,2,3,4,5], k = 2
-Output: [4,5,1,2,3]
+  Input: head = [1,2,3,4,5], k = 2
+  Output: [4,5,1,2,3]
 
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- * @param {ListNode} head
- * @param {number} k
- * @return {ListNode}}
- */
+  * Definition for singly-linked list.
+  * function ListNode(val, next) {
+  *     this.val = (val===undefined ? 0 : val)
+  *     this.next = (next===undefined ? null : next)
+  * }
+  * @param {ListNode} head
+  * @param {number} k
+  * @return {ListNode}
+}
+*/
  var rotateRight = (head, k) => {
     if(!head) return head;
     if(size(head) === 1) return head;  
