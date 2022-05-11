6ede74202a7e0e9376ff140f211176bcecfd0dc0
Flatout007 <thechamp301@gmail.com>
2022-05-11 17:02:29 -0400

    217. Contains Duplicate[Easy]

--- leetcode_217.js
+++ leetcode_217.js
@@ -1,31 +1,40 @@
 /**
- * 217. Contains Duplicate[Easy]
+ @description @namespace {
+    217. Contains Duplicate[Easy]
 
-Given an integer array nums, return true if any value appears 
-at least twice in the array, and return false if every element is distinct.
-
-Example 1:
-Input: nums = [1,2,3,1]
-Output: true
- */
-/**
+    Given an integer array nums, return true if any value appears 
+    at least twice in the array, and return false if every element is distinct.
+ }
+@example {
+    Input: nums = [1,2,3,1]
+    Output: true
+}
  * @param {number[]} nums
  * @return {boolean}
- * A program that returns true if an Array cell contains the same 
- * number more than once, else returns false
  */
- var containsDuplicate = function(nums) {
-    // 1. go through nums, with a seen array
-    // 2. if the cell in seen array at index nums[i] is seen again 
-    // return false
-    // 3. check if the number is seen before, by storing true 
-    // in seen array at index nums[i]
-    let seen = []; 
+var containsDuplicate = function (nums) {
+  /**
+    @algorithm {
+    1.SET a duplicate checker array filled with n false values;
     
-    for(let i = 0; i<nums.length; i++) {
-        if(seen[nums[i]]) return true;
-        seen[nums[i]] = true;
+    2.TRAVERSE nums;
+    
+    2.CHECK & Evaluate 1 statement while traversing!
+      IF curr nums[i] is true in duplicate checker array;
+      RETURN true because its a duplicate;
+      ELSE set duplicate checker of curr nums[i] to true; 
     }
-    
-    return false;
+    */
+
+  let dupCheckr = new Array(nums.length + 1).fill(false);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (dupCheckr[nums[i]] === true) {
+      return true;
+    }
+
+    dupCheckr[nums[i]] = true;
+  }
+
+  return false;
 };
